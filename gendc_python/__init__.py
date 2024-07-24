2a0f10c937a3808a2722faa9404a1ec9f204aa43
Fixstars-momoko <Fixstars-momoko@users.noreply.github.com>
2024-07-24 21:21:59 +0000

    Update gendc-pyton module version to v0.2.10 by Release workflow

--- gendc_python/__init__.py
+++ gendc_python/__init__.py
@@ -1,2 +1,6 @@
 __version__ = 'X.X.X' #this will be updated by cicd
 from . import gendc_separator
+
+__version__ = '0.2.10' # this will be automaticall updated by cicd
+from . import gendc_separator
+
